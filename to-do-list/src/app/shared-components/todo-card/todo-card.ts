fb39f5ebef1383cfa07f6b6b81783da2f967793e
Joel Araújo <joel_saraujo2310@hotmail.com>
2026-07-03 22:38:57 +0100

    Before angular update

--- to-do-list/src/app/shared-components/todo-card/todo-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-todo-card',
-  imports: [],
-  templateUrl: './todo-card.html',
-  styleUrl: './todo-card.css',
-})
-export class TodoCard {}
