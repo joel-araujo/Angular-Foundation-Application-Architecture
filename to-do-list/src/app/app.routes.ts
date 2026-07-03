cc9d49d19aa9cc78e0243275eb91100abee900f4
Joel Araújo <joel_saraujo2310@hotmail.com>
2026-07-03 13:02:10 +0100

    Routing updates

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { ToDoList } from './components/to-do-list/to-do-list';
+import { ToDoDetail } from './components/to-do-detail/to-do-detail';
+import { ToDoCreateTask } from './components/to-do-create-task/to-do-create-task';
 
 export const routes: Routes = [
-    
+    {path:'', redirectTo: 'home', pathMatch: 'full'},
+    {path:'home', component: Home},    
+    {path:'toDoList', component: ToDoList},    
+    {path:'toDoDetail', component: ToDoDetail},    
+    {path:'toDoCreateTask', component: ToDoCreateTask},
+    {path:'**', component: Home}    
 ];
