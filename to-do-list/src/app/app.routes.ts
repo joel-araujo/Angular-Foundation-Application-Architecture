e873fce3438f96a6a21416d2b6446099f41ca85d
Joel Araújo <joel_saraujo2310@hotmail.com>
2026-07-04 10:53:05 +0100

    Remove todo list midle handler

--- to-do-list/src/app/app.routes.ts
+++ to-do-list/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { ToDoList } from './components/to-do-list/to-do-list';
 import { ToDoDetail } from './components/to-do-detail/to-do-detail';
 import { ToDoCreateTask } from './components/to-do-create-task/to-do-create-task';
 
@@ -8,12 +7,9 @@ export const routes: Routes = [
     {path:'', redirectTo: 'home', pathMatch: 'full'},
     {path:'home', component: Home,
         children: [
-            {path:'toDoList', component: ToDoList},    
             {path:'toDoDetail', component: ToDoDetail},    
-            {path:'toDoCreateTask', component: ToDoCreateTask},
         ]
     },
-    {path:'toDoList', component: ToDoList},    
     {path:'toDoDetail', component: ToDoDetail},    
     {path:'toDoCreateTask', component: ToDoCreateTask},
     {path:'**', component: Home}    
--- to-do-list/src/app/components/home/home.ts
+++ to-do-list/src/app/components/home/home.ts
@@ -1,8 +1,11 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { TaskInterface, TaskService } from '../../services/task';
+import { CommonModule } from '@angular/common';
+import { TaskCard } from '../task-card/task-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, TaskCard],
   templateUrl: './home.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './home.css',
@@ -10,5 +13,21 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 
 export class Home {
+  constructor(public task:  TaskService) {}
 
+  tasks: TaskInterface[] = [];
+
+  ngOnInit() {
+  this.tasks = this.task.getTasks();
+  }
+
+  toggleTask(id: number) {
+  this.task.toggleTask(id);
+  this.tasks = this.task.getTasks();
+  }
+
+  deleteTask(id: number) {
+  this.task.deleteTask(id);
+  this.tasks = this.task.getTasks();
+  }
 }
